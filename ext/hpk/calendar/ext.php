64bf470334e45c490e9b2246883a0244c7a4b03e
Błażej <blazejhap@gmail.com>
2015-02-18 01:12:44 +0100

    more work

---
+++ ext/hpk/calendar/ext.php
@@ -0,0 +1,17 @@
+<?php
+/**
+*
+* @package phpBB Extension - Calendar
+* @copyright (c) 2015 Błażej Hap - HPK
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace hpk\calendar;
+/**
+* @ignore
+*/
+
+class ext extends \phpbb\extension\base
+{
+}
